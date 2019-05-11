f8c2468bdc237a753f00231f1ef6e844744a3ca1
Diego Mais <diegomais@live.com>
2019-05-11 12:07:35 -0300

    Feat: Provide basic Express setup

---
+++ server.js
@@ -0,0 +1,18 @@
+// Import Express module: web application framework.
+const express = require('express');
+
+// Create app object by calling the top-level express() function exported by Express module.
+const app = express();
+
+/** Respond with 'Hello WeDev' when a GET request is made to the root URL (/) or route.
+ * The req (request) and res (response) are the exact same objects that Node provides, so we can
+ * invoke req.pipe(), req.on('data', callback), and anything else we would do without Express
+ * involved. */
+app.get('/', (req, res) => res.send('Hello WeDev!'));
+
+/** Create a constant reference to port declared on environment variables
+ * or set the port of remote server to 3001.  */
+const PORT = process.env.PORT || 3001;
+
+// Bind and listen for connections on the specified port.
+app.listen(PORT, () => console.log(`Server started on port ${PORT}.`));
