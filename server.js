416a4f3abdd3d55a567432fe65de83165a3bc132
Diego Mais <diegomais@live.com>
2019-05-13 20:28:42 -0300

    Feat: Provide connection to MongoDB with Mongoose

--- server.js
+++ server.js
@@ -1,8 +1,17 @@
 // Import Express module: web application framework.
 const express = require('express');
 
+// Import dotenv module: load environment variables from '.env'.
+const dotenv = require('dotenv').config();
+
+// Import database configurations
+const connectDB = require('./config/db');
+
 // Create app object by calling the top-level express() function exported by Express module.
 const app = express();
+
+// Connect Database
+connectDB();
 
 /** Respond with 'Hello WeDev' when a GET request is made to the root URL (/) or route.
  * The req (request) and res (response) are the exact same objects that Node provides, so we can
