9ebc815040f710c272a766030e2c98ca7939d166
Diego Mais <diegomais@live.com>
2019-05-15 22:22:27 -0300

    Feat: Provide user registration

--- routes/api/user.js
+++ routes/api/user.js
@@ -1,9 +1,82 @@
+// Import Express module: web application framework.
 const express = require('express');
+
+// Creates a new router object.
 const router = express.Router();
 
-// @route    GET api/users
-// @desc     Test route
+// Import express-validator module: middlewares that wraps validator.js functions.
+const { check, validationResult } = require('express-validator/check');
+
+// Import Gravatar module: library to generate Gravatar URLs in Node.js Based on gravatar specs.
+const gravatar = require('gravatar');
+
+// Import bcrypt module: library to hash passwords.
+const bcrypt = require('bcryptjs');
+
+// Load User model.
+const User = require('../../models/User');
+
+// @route    POST api/user
+// @desc     Register user
 // @access   Public
-router.get('/', (req, res) => res.send('User route'));
+router.post(
+  '/',
+  [
+    // Verify if name exists in request.
+    check('name', 'Your name is required.')
+      .not()
+      .isEmpty(),
+    // Verify if email must be an email.
+    check('email', 'Please provide a valid email address.').isEmail(),
+    // Verify if password must be at least 6 chars long.
+    check(
+      'password',
+      'Your password must be at least 6 characters in length.'
+    ).isLength({ min: 6 })
+  ],
+  async (req, res) => {
+    // Finds the validation errors in this request and wraps them in an object with handy functions
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(422).json({ errors: errors.array() });
+    }
+
+    // Destructuring assignment from request.
+    const { name, email, password } = req.body;
+
+    try {
+      // Asynchronously finds e-mail and check if e-mail is in use
+      let user = await User.findOne({ email });
+      if (user) {
+        return res.status(409).json({
+          errors: [{ message: 'E-mail already in use.' }]
+        });
+      }
+
+      // Request Gravatar image
+      const avatar = gravatar.url(email, { s: '200', d: 'mp' });
+
+      // Construct Document: new instance of User model.
+      user = new User({ name, email, avatar, password });
+
+      // Asynchronously generates a salt.
+      const salt = await bcrypt.genSalt(10);
+
+      // Asynchronously generates a hash for the given string.
+      user.password = await bcrypt.hash(password, salt);
+
+      // Asynchronously save user document to database.
+      await user.save();
+
+      // TODO: Return JSON Web Token
+
+      // Send message: 'User registered.'
+      res.send('User registered.');
+    } catch (error) {
+      console.error(error.message);
+      res.status(500).send('Server error');
+    }
+  }
+);
 
 module.exports = router;
