23f471da1104a057fd0427841c432994995f45fb
Diego Mais <diegomais@live.com>
2019-05-22 23:58:20 -0300

    Add remove experience from profile api
    This feature provides user to remove experiences from user profile.
    	modified:   routes/api/profile.js

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -261,4 +261,34 @@ router.put(
   }
 );
 
+// @route    DELETE api/profile/experience/:exp_id
+// @desc     Delete experience from profile
+// @access   Private
+router.delete('/experience/:exp_id', auth, async (req, res) => {
+  try {
+    // Find profile on database using id from user authenticated.
+    const profile = await Profile.findOne({ user: req.user.id });
+
+    // Get index of the experience to remove from experience array.
+    const removeIndex = profile.experience
+      .map(item => item.id)
+      .indexOf(req.params.exp_id);
+
+    // Remove the index finded above from experience array.
+    profile.experience.splice(removeIndex, 1);
+
+    // Save the changes on database.
+    await profile.save();
+
+    // Return the updated profile.
+    return res.json(profile);
+  } catch (error) {
+    // Outputs the error message to the Web Console.
+    console.error(error.message);
+    // Return 500 Internal Server Error response code: Indicates that the server encountered an
+    // unexpected condition that prevented it from fulfilling the request. */
+    return res.status(500).send('Internal Server Error.');
+  }
+});
+
 module.exports = router;
