d1ed5d260673c39590258ad002eed9e22c2200f6
Diego Mais <diegomais@live.com>
2019-05-25 07:36:28 -0300

    Add create new post API
    - This feature provides user to create a new post on database.
    
    Changes to be committed:
    	modified:   models/Post.js
    	modified:   routes/api/posts.js

--- models/Post.js
+++ models/Post.js
@@ -1,12 +1,16 @@
 // Import mongoose package: mongodb object modeling for node.js.
 const mongoose = require('mongoose');
 
+// Everything in Mongoose starts with a Schema. Each schema maps to a MongoDB collection
+// and defines the shape of the documents within that collection.
+const Schema = mongoose.Schema;
+
 /** Define post schema that defines the shape of the documents within posts collection.
  * user: A DBRef (database reference) is an ObjectID referencing an object in users collection.
  * name and avatar: Provide display name and avatar of publisher in case of user accout removed.
  * likes: Array of DBRef (database reference) referencing an object in users collection.
  * comments: Array of nested documents. */
-const PostSchema = new mongoose.Schema({
+const PostSchema = new Schema({
   user: { type: Schema.Types.ObjectId, ref: 'users' },
   name: { type: String },
   avatar: { type: String },
--- routes/api/posts.js
+++ routes/api/posts.js
@@ -1,9 +1,68 @@
+// Import Express package: web application framework.
 const express = require('express');
+
+// Create a new router object.
 const router = express.Router();
 
-// @route    GET api/posts
-// @desc     Test route
-// @access   Public
-router.get('/', (req, res) => res.send('Posts route'));
+// Load authentication middleware.
+const auth = require('../../middleware/auth');
+
+// Import express-validator module: middlewares that wraps validator.js functions.
+const { check, validationResult } = require('express-validator/check');
+
+// Load User model.
+const User = require('../../models/User');
+
+// Load Post model.
+const Post = require('../../models/Post');
+
+// @route    POST api/posts
+// @desc     Create a post
+// @access   Private
+router.post(
+  '/',
+  [
+    auth,
+    [
+      // Validate if text exists in request.
+      check('text', 'Text is required.')
+        .not()
+        .isEmpty()
+    ]
+  ],
+  async (req, res) => {
+    // Find the validation errors in this request and wraps them in an object with handy functions.
+    const errors = validationResult(req);
+    if (!errors.isEmpty())
+      return res.status(400).json({
+        errors: errors.array()
+      });
+
+    try {
+      // Find user document by id from header.
+      const user = await User.findById(req.user.id);
+
+      // Build newPost object.
+      const newPost = new Post({
+        user: req.user.id,
+        name: user.name,
+        avatar: user.avatar,
+        text: req.body.text
+      });
+
+      // Save the changes on database.
+      const post = await newPost.save();
+
+      // Return post saved on database.
+      return res.json(post);
+    } catch (error) {
+      // Outputs the error message to the Web Console.
+      console.error(error.message);
+      // Return 500 Internal Server Error response code: Indicates that the server encountered an
+      // unexpected condition that prevented it from fulfilling the request. */
+      return res.status(500).send('Internal Server Error.');
+    }
+  }
+);
 
 module.exports = router;
