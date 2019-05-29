23076abc827ce6f8a88f303e6c7085bc4754181b
Diego Mais <diegomais@live.com>
2019-05-28 23:43:25 -0300

    Add register page UI
    - This feature provides register page UI.
    
    Changes to be committed:
    	modified:   client/package.json
    	modified:   client/yarn.lock
    	modified:   client/src/App.js
    	modified:   client/src/components/layout/Landing.js
    	modified:   client/src/components/layout/Navbar.js
    	new file:   client/src/components/auth/Login.js
    	new file:   client/src/components/auth/Register.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,25 @@
 import React, { Fragment } from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import Navbar from './components/layout/Navbar';
 import Landing from './components/layout/Landing';
+import Register from './components/auth/Register';
+import Login from './components/auth/Login';
 import './App.css';
 
 const App = () => (
-  <Fragment>
-    <Navbar />
-    <Landing />
-  </Fragment>
+  <Router>
+    <Fragment>
+      <Navbar />
+      <Route exact path='/' component={Landing} />
+      {/** This section provides the content of page is pushed to the middle. */}
+      <section className='container'>
+        <Switch>
+          <Route exact path='/register' component={Register} />
+          <Route exact path='/login' component={Login} />
+        </Switch>
+      </section>
+    </Fragment>
+  </Router>
 );
 
 export default App;
---
+++ client/src/components/auth/Register.js
@@ -0,0 +1,77 @@
+import React, { Fragment, useState } from 'react';
+import { Link } from 'react-router-dom';
+
+const Register = () => {
+  const [formData, setFormData] = useState({
+    name: '',
+    email: '',
+    password: '',
+    password2: ''
+  });
+
+  const { name, email, password, password2 } = formData;
+
+  const onChange = e =>
+    setFormData({ ...formData, [e.target.name]: e.target.value });
+
+  return (
+    <Fragment>
+      <h1 className='large text-primary'>Sign Up</h1>
+      <p className='lead'>
+        <i className='fas fa-user' /> Create Your Account
+      </p>
+      <form className='form'>
+        <div className='form-group'>
+          <input
+            type='text'
+            placeholder='Name'
+            name='name'
+            value={name}
+            onChange={e => onChange(e)}
+          />
+        </div>
+        <div className='form-group'>
+          <input
+            type='email'
+            placeholder='Email Address'
+            name='email'
+            value={email}
+            onChange={e => onChange(e)}
+          />
+          <small className='form-text'>
+            This site uses Gravatar, so if you want a profile image, use a
+            Gravatar email.
+          </small>
+        </div>
+        <div className='form-group'>
+          <input
+            type='password'
+            placeholder='Password'
+            name='password'
+            value={password}
+            minlength='6'
+            onChange={e => onChange(e)}
+          />
+        </div>
+        <div className='form-group'>
+          <input
+            type='password'
+            placeholder='Confirm password'
+            name='password2'
+            value={password2}
+            minlength='6'
+            onChange={e => onChange(e)}
+          />
+        </div>
+        <div className='form-group'>
+          <input type='submit' value='Register' className='btn btn-primary' />
+        </div>
+      </form>
+      <p className='my-1'>
+        Already have an account? <Link to='/login'>Sign In</Link>
+      </p>
+    </Fragment>
+  );
+};
+
+export default Register;
