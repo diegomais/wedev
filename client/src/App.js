207dd770e29746a854f3d82e53456ec7f3559c11
Diego Mais <diegomais@live.com>
2019-05-29 23:46:13 -0300

    Add Redux store and Redux DevTools extension
    - Redux provides predictable state container for JavaScript apps and
      helps to write applications that behave consistently.
    - The extension Redux DevTools provides power-ups for Redux development
      workflow with debugging application's state changes.
    
    Changes to be committed:
    	modified:   client/package.json
    	modified:   client/yarn.lock
    	modified:   client/src/App.js
    	new file:   client/src/redux/store.js
    	new file:   client/src/redux/ducks/index.js

--- client/src/App.js
+++ client/src/App.js
@@ -4,22 +4,27 @@ import Navbar from './components/layout/Navbar';
 import Landing from './components/layout/Landing';
 import Register from './components/auth/Register';
 import Login from './components/auth/Login';
+// Redux
+import { Provider } from 'react-redux';
+import store from './redux/store';
 import './App.css';
 
 const App = () => (
-  <Router>
-    <Fragment>
-      <Navbar />
-      <Route exact path='/' component={Landing} />
-      {/** This section provides the content of page is pushed to the middle. */}
-      <section className='container'>
-        <Switch>
-          <Route exact path='/register' component={Register} />
-          <Route exact path='/login' component={Login} />
-        </Switch>
-      </section>
-    </Fragment>
-  </Router>
+  <Provider store={store}>
+    <Router>
+      <Fragment>
+        <Navbar />
+        <Route exact path='/' component={Landing} />
+        {/** This section provides the content of page is pushed to the middle. */}
+        <section className='container'>
+          <Switch>
+            <Route exact path='/register' component={Register} />
+            <Route exact path='/login' component={Login} />
+          </Switch>
+        </section>
+      </Fragment>
+    </Router>
+  </Provider>
 );
 
 export default App;
