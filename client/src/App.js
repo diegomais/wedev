8b25ece5ead0e7825d349b155cb7b42115bd831b
Diego Mais <diegomais@live.com>
2019-06-01 00:20:27 -0300

    Add load user and set authorization token
    - This feature provides to load user data from database and set authorization token in headers.
    Changes to be committed:
    	modified:   client/src/redux/ducks/auth.js
    	new file:   client/src/utils/setAuthToken.js
    	modified:   client/src/App.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,4 @@
-import React, { Fragment } from 'react';
+import React, { Fragment, useEffect } from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import Navbar from './components/layout/Navbar';
 import Landing from './components/layout/Landing';
@@ -8,25 +8,34 @@ import Login from './components/auth/Login';
 // Redux
 import { Provider } from 'react-redux';
 import store from './redux/store';
+import { loadUser, TOKEN_KEY } from './redux/ducks/auth';
+import setAuthToken from './utils/setAuthToken';
 import './App.css';
 
-const App = () => (
-  <Provider store={store}>
-    <Router>
-      <Fragment>
-        <Navbar />
-        <Route exact path='/' component={Landing} />
-        {/** This section provides the content of page is pushed to the middle. */}
-        <section className='container'>
-          <Alert />
-          <Switch>
-            <Route exact path='/register' component={Register} />
-            <Route exact path='/login' component={Login} />
-          </Switch>
-        </section>
-      </Fragment>
-    </Router>
-  </Provider>
-);
+if (localStorage[TOKEN_KEY]) setAuthToken(localStorage[TOKEN_KEY]);
+
+const App = () => {
+  useEffect(() => {
+    store.dispatch(loadUser());
+  }, []);
+  return (
+    <Provider store={store}>
+      <Router>
+        <Fragment>
+          <Navbar />
+          <Route exact path='/' component={Landing} />
+          {/** This section provides the content of page is pushed to the middle. */}
+          <section className='container'>
+            <Alert />
+            <Switch>
+              <Route exact path='/register' component={Register} />
+              <Route exact path='/login' component={Login} />
+            </Switch>
+          </section>
+        </Fragment>
+      </Router>
+    </Provider>
+  );
+};
 
 export default App;
--- client/src/redux/ducks/auth.js
+++ client/src/redux/ducks/auth.js
@@ -1,15 +1,18 @@
 import axios from 'axios';
 import { setAlert } from './alert';
+import setAuthToken from '../../utils/setAuthToken';
 
 // Action Types
 const REGISTER_SUCCESS = 'wedev/auth/REGISTER_SUCCESS';
 const REGISTER_FAIL = 'wedev/auth/REGISTER_FAIL';
+const USER_LOADED = 'wedev/auth/USER_LOADED';
+const AUTH_ERROR = 'wedev/auth/AUTH_ERROR';
 
 // Reducer
-const tokenKey = 'wedev/auth/token';
+export const TOKEN_KEY = '@wedev-Token';
 
 const initialState = {
-  token: localStorage.getItem(tokenKey),
+  token: localStorage.getItem(TOKEN_KEY),
   isAuthenticated: null,
   loading: true,
   user: null
@@ -20,11 +23,19 @@ export default function reducer(state = initialState, action = {}) {
 
   switch (type) {
     case REGISTER_SUCCESS:
-      localStorage.setItem(tokenKey, payload.token);
+      localStorage.setItem(TOKEN_KEY, payload.token);
       return { ...state, ...payload, isAuthenticated: true, loading: false };
     case REGISTER_FAIL:
-      localStorage.removeItem(tokenKey);
+    case AUTH_ERROR:
+      localStorage.removeItem(TOKEN_KEY);
       return { ...state, token: null, isAuthenticated: false, loading: false };
+    case USER_LOADED:
+      return {
+        ...state,
+        isAuthenticated: true,
+        loading: false,
+        user: payload.user
+      };
     default:
       return state;
   }
@@ -63,5 +74,18 @@ export const registerUser = ({ name, email, password }) => async dispatch => {
       );
 
     dispatch({ type: REGISTER_FAIL });
+  }
+};
+
+// Load User
+export const loadUser = () => async dispatch => {
+  if (localStorage[TOKEN_KEY]) setAuthToken(localStorage[TOKEN_KEY]);
+
+  try {
+    const res = await axios.get('/api/auth');
+
+    dispatch({ type: USER_LOADED, payload: { user: res.data } });
+  } catch (error) {
+    dispatch({ type: AUTH_ERROR });
   }
 };
