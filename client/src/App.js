b80956aecac01c4c51883dbc78a842e56fe588c8
Diego Mais <diegomais@live.com>
2019-06-02 20:24:19 -0300

    Add create profile form UI
    - This feature provides UI to create a profile.
    Changes to be committed:
    	new file:   client/src/components/profile-forms/CreateProfile.js
    	modified:   client/src/App.js

--- client/src/App.js
+++ client/src/App.js
@@ -7,6 +7,7 @@ import Register from './components/auth/Register';
 import Login from './components/auth/Login';
 import PrivateRoute from './components/routing/PrivateRoute';
 import Dashboard from './components/dashboard/Dashboard';
+import CreateProfile from './components/profile-forms/CreateProfile';
 
 // Redux
 import { Provider } from 'react-redux';
@@ -34,6 +35,11 @@ const App = () => {
               <Route exact path='/register' component={Register} />
               <Route exact path='/login' component={Login} />
               <PrivateRoute exact path='/dashboard' component={Dashboard} />
+              <PrivateRoute
+                exact
+                path='/create-profile'
+                component={CreateProfile}
+              />
             </Switch>
           </section>
         </Fragment>
