60e87c8456ed009205c9a357c0f4a0023791ab1a
Diego Mais <diegomais@live.com>
2019-06-02 18:58:25 -0300

    Add redirect to dashboard if user is logged in landing page.
    - This feature provides user to redirect automatically to dashboard when logged in.
    Changes to be committed:
    	modified:   client/src/components/layout/Landing.js

--- client/src/components/layout/Landing.js
+++ client/src/components/layout/Landing.js
@@ -1,7 +1,11 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+import { Link, Redirect } from 'react-router-dom';
+import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
 
-const Landing = () => {
+const Landing = ({ isAuthenticated }) => {
+  if (isAuthenticated) return <Redirect to='/dashboard' />;
+
   return (
     <section className='landing'>
       <div className='dark-overlay'>
@@ -25,4 +29,12 @@ const Landing = () => {
   );
 };
 
-export default Landing;
+Landing.propTypes = {
+  isAuthenticated: PropTypes.bool
+};
+
+const mapStateToProps = state => ({
+  isAuthenticated: state.auth.isAuthenticated
+});
+
+export default connect(mapStateToProps)(Landing);
