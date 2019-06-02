d8d2bb416836a9786df73f26fd6755ed175c9fab
Diego Mais <diegomais@live.com>
2019-06-02 18:32:57 -0300

    Add profile reducer and getCurrentProfile actiomn
    - This feature provides user to get the profile of current user.
    Changes to be committed:
    	new file:   client/src/redux/ducks/profile.js
    	modified:   client/src/redux/ducks/index.js
    	modified:   client/src/components/dashboard/Dashboard.js

--- client/src/redux/ducks/index.js
+++ client/src/redux/ducks/index.js
@@ -2,7 +2,8 @@ import { combineReducers } from 'redux';
 
 import alert from './alert';
 import auth from './auth';
+import profile from './profile';
 
-const rootReducer = combineReducers({ alert, auth });
+const rootReducer = combineReducers({ alert, auth, profile });
 
 export default rootReducer;
---
+++ client/src/redux/ducks/profile.js
@@ -0,0 +1,47 @@
+import axios from 'axios';
+
+// Action Types
+const GET_PROFILE = 'wedev/profile/GET_PROFILE';
+const PROFILE_ERROR = 'wedev/profile/PROFILE_ERROR';
+
+// Reducer
+const initialState = {
+  profile: null,
+  profiles: [],
+  repos: [],
+  loading: true,
+  error: {}
+};
+export default function reducer(state = initialState, action = {}) {
+  const { type, payload } = action;
+
+  switch (type) {
+    case GET_PROFILE:
+      return { ...state, profile: payload.profile, loading: false };
+    case PROFILE_ERROR:
+      return { ...state, error: payload.error, loading: false };
+    default:
+      return state;
+  }
+}
+
+// Action Creators
+
+// Get current users profile
+export const getCurrentProfile = () => async dispatch => {
+  try {
+    const res = await axios.get('/api/profile/me');
+
+    dispatch({ type: GET_PROFILE, payload: { profile: res.data } });
+  } catch (error) {
+    dispatch({
+      type: PROFILE_ERROR,
+      payload: {
+        error: {
+          message: error.response.statusText,
+          status: error.response.status
+        }
+      }
+    });
+  }
+};
