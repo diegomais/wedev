2ca24a0cee81582423f625f132c0bc2b80590c14
Diego Mais <diegomais@live.com>
2019-06-02 23:07:56 -0300

    Add edit profile
    - This feature provides user to edit your profile.
    
    Changes to be committed:
    	new file:   client/src/components/profile-forms/EditProfile.js
    	modified:   client/src/components/dashboard/Dashboard.js
    	modified:   client/src/App.js

--- client/src/App.js
+++ client/src/App.js
@@ -8,6 +8,7 @@ import Login from './components/auth/Login';
 import PrivateRoute from './components/routing/PrivateRoute';
 import Dashboard from './components/dashboard/Dashboard';
 import CreateProfile from './components/profile-forms/CreateProfile';
+import EditProfile from './components/profile-forms/EditProfile';
 
 // Redux
 import { Provider } from 'react-redux';
@@ -39,6 +40,11 @@ const App = () => {
                 exact
                 path='/create-profile'
                 component={CreateProfile}
+              />
+              <PrivateRoute
+                exact
+                path='/edit-profile'
+                component={EditProfile}
               />
             </Switch>
           </section>
--- client/src/components/dashboard/Dashboard.js
+++ client/src/components/dashboard/Dashboard.js
@@ -22,8 +22,22 @@ const Dashboard = ({
       <p className='lead'>
         <i className='fas fa-user' /> Welcome {user && user.name}
       </p>
+
       {profile !== null ? (
-        <Fragment>{JSON.stringify(profile)}</Fragment>
+        <Fragment>
+          <div className='dash-buttons'>
+            <Link to='/edit-profile' className='btn btn-light'>
+              <i className='fas fa-user-circle text-primary' /> Edit Profile
+            </Link>
+            <Link to='/add-experience' className='btn btn-light'>
+              <i className='fab fa-black-tie text-primary' /> Add Experience
+            </Link>
+            <Link to='/add-education' className='btn btn-light'>
+              <i className='fas fa-graduation-cap text-primary' /> Add Education
+            </Link>
+          </div>
+          {JSON.stringify(profile)}
+        </Fragment>
       ) : (
         <Fragment>
           <p>You don't have a profile yet, let's create your profile now?</p>
