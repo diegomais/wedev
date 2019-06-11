ca860da81a7b8b1cec5edf04cd5551f9aa24739f
Diego Mais <diegomais@live.com>
2019-06-10 23:16:44 -0300

    Prepare for deployment
    - This features serve React app from Node server.
    
    Changes to be committed:
    	modified:   package.json
    	modified:   yarn.lock
    	modified:   server.js

--- server.js
+++ server.js
@@ -7,6 +7,9 @@ const dotenv = require('dotenv').config();
 // Import database configurations
 const connectDB = require('./config/db');
 
+// Import NodeJS 'path' module.
+const path = require('path');
+
 // Create app object by calling the top-level express() function exported by Express module.
 const app = express();
 
@@ -23,6 +26,16 @@ app.use('/api/users', require('./routes/api/users'));
 app.use('/api/auth', require('./routes/api/auth'));
 app.use('/api/profile', require('./routes/api/profile'));
 app.use('/api/posts', require('./routes/api/posts'));
+
+// Serve static assets in production
+if (process.env.NODE_ENV === 'production') {
+  // Set static folder
+  app.use(express.static('client/build'));
+  // Serve React app from Node server
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  });
+}
 
 /** Create a constant reference to port declared on environment variables
  * or set the port of remote server to 3001.  */
